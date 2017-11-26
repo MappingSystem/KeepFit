424b7cf6d48967406c8b8134c9bb2e389a85f20d
Nuno M <greven.pt@gmail.com>
2017-11-26 11:26:45 +0000

    Clean up

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import createPersistedState from 'vuex-persistedstate'
 import actions from './actions'
 import mutations from './mutations'
 import getters from './getters'
@@ -7,6 +8,7 @@ import getters from './getters'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // plugins: [createPersistedState()],
   state: {
     items: [/* [id: number]: Item */],
     goals: {/* [type: string]: Goal */}

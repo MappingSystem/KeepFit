b8a04f1853badb6facc12fee8a38372a34adb5fe
Nuno M <greven.pt@gmail.com>
2017-11-27 11:56:40 +0000

    Remove unneeded getter

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,11 +4,8 @@ export default {
   goals: state => {
     return state.goals
   },
-  items: state => {
-    return state.items
-  },
   // items sorted by dates, latest to oldest
-  sortedItems: state => {
+  items: state => {
     return state.items.sort((a, b) => {
       return parseDate(b.date) - parseDate(a.date)
     })
